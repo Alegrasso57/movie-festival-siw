42934671b5e9ed7e84f2cb7a23b02658d77129e5
alegrasso3 <ale.grasso3@stud.uniroma3.it>
2026-09-03 17:15:42 +0200

    chore: setup iniziale progetto Spring Boot con connessione a Supabase funzionante

---
+++ src/main/java/it/uniroma3/siw/moviefestival/MoviefestivalApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.moviefestival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviefestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviefestivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/moviefestival/MoviefestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.moviefestival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviefestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
